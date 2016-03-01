57a5ea7591d3e291f1965eddb8a10196c9d1029b
younglau182 <georgeliu182@gmail.com>
2016-03-01 14:54:07 +0800

    modified:   _posts/2016-03-01-net3.5-setup.md  	deleted:    download/Win8.win8.1.win10.net3.5.setup.bat

--- download/Win8.win8.1.win10.net3.5.setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo.
-set /p Image=ÊäÈëWin8/win8.1/win10¾µÏñĞéÄâ¹âÇıÅÌ·û:
-%Image%:\sources\dism.exe /online /enable-feature /featurename:NetFX3 /Source:%Image%:\sources\sxs
-echo
-pause
